ba1f51a6cd782062eb114c6d024a002e05183ef4
zhouxiaojie <zhouxj@chinatelecom.cn>
2017-06-09 10:23:24 +0800

    delete all

--- _posts/2017-06-07-welcome-back.markdown
+++
@@ -1,8 +0,0 @@
----
-layout: post
-title:  "Welcome back"
-date:   2017-06-07 12:40:35
-tag : test
-categories: jekyll update
----
-i'm back
--- assets/themes/js/logging.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-
-	$.get('http://www.templateapi.com/themes/log?id='+858081+'&oi='+2138+'&ot=1&&url='+window.location, function(json){})    
-
-});
